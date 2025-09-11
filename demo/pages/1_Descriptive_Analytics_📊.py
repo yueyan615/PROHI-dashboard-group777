bca8ee5aa47aef41511bf0fad57ac27d1a1544ce
yueyan615 <yueyan.li@outlook.com>
2025-09-11 13:38:28 +0200

    1

--- demo/pages/1_Descriptive_Analytics_📊.py
+++ demo/pages/1_Descriptive_Analytics_📊.py
@@ -1,5 +1,20 @@
 import streamlit as st
 
+
+img1 = 'logo_nb.png'
+img2 = 'logo2.png'
+img3 = 'logo3.png'
+
+
+### Logo
+img1 = 'logo_nb.png'
+st.logo(img1, size= "large", icon_image=None)  
+#######################################
+
+
+
+
+
 st.markdown("# Descriptive Analytics 📊")
 
 st.sidebar.markdown("# Descriptive Analytics 📊")
---
+++ demo/pages/2_Diagnostic analytics_.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+img1 = 'logo_nb.png'
+img2 = 'logo2.png'
+img3 = 'logo3.png'
+
+
+### Logo
+img1 = 'logo_nb.png'
+st.logo(img1, size= "large", icon_image=None)  
+#######################################
