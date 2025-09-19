2bd3dd1c6181ca80c3303e64db6741aa93128063
KKQQ <tana_mana@foxmail.com>
2025-09-19 15:42:35 +0200

    Update 5_ℹ️About.py

--- pages/5_ℹ️About.py
+++ pages/5_ℹ️About.py
@@ -1,6 +1,5 @@
-import streamlit as st
 import pandas as pd
-
+import streamlit as st
 
 st.set_page_config(
     # page_title="Obesity Dashboard",
@@ -22,16 +21,33 @@ st.logo(img1, size= "large", icon_image=None)
 """
 # About the Dataset 
 """
-"""
-Description ...
-"""
+
+st.markdown("""
+This dashboard uses data from the [UCI Machine Learning Repository](https://archive.ics.uci.edu/dataset/544/estimation+of+obesity+levels+based+on+eating+habits+and+physical+condition).
+
+**Dataset name:** *Estimation of Obesity Levels Based on Eating Habits and Physical Condition*  
+
+**Description:**  
+The dataset contains information collected from individuals regarding their **eating habits**, **physical condition**, and **lifestyle behaviors**.  
+It was designed to predict obesity levels, classified into categories such as *Underweight, Normal Weight, Overweight, and Obese* based on WHO standards.  
+""")
 
 """
-# About Our Team 
+# About Our Team
 """
-"""
-Description ...
-"""
+st.markdown("""
+This dashboard was created by **Group 777** as part of assignemtn of Project Management and Tools for Health Informatics (PROHI).
+The team members are:
+Weiqi Kong 
+
+Yueyan Li 
+
+Zsolt Fehér 
+
+Christoffer Brändefors 
+
+Naznin Akhtar
+""")
 
 # st.code("x = 2021")
 
